3261082ca95bc535c27b04d6ea336117563f2a0d
volodymyr.romaniv <v.b.romaniv@gmail.com>
2022-08-23 18:46:27 +0300

    Added basic argument parser

---
+++ github-dorker/ArgumentParser.cs
@@ -0,0 +1,24 @@
+﻿namespace GithubDorker
+{
+    public class ArgumentParser
+    {
+        public Dictionary<string, List<string>> ParseArguments(string[] arguments)
+        {
+            var result = new Dictionary<string, List<string>>();
+
+            foreach (var item in arguments)
+            {
+                if (item.StartsWith('-') || item.StartsWith("--"))
+                {
+                    result.Add(item, new List<string>());
+                }
+                else if (result.Keys.Any())
+                {
+                    result.Last().Value.Add(item);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ github-dorker/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GithubDorker
+{
+    class GithubDorker
+    {
+        static async Task Main(string[] args)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("GH_TOKEN");
+            var parameters = Environment.GetCommandLineArgs();
+            var argumentParser = new ArgumentParser();
+            var parsedArgs = argumentParser.ParseArguments(args);
+
+            foreach (var item in parsedArgs)
+            {
+                Console.WriteLine($"{item.Key}, [{string.Join(',', item.Value)}]");
+            }
+        }
+    }
+}
