d0f04da093e038ffdde71f44b66f4fec5e79febf
volodymyr.romaniv <v.b.romaniv@gmail.com>
2022-08-24 10:44:28 +0300

    Implemented basic batched tasks for GitHub requesting

--- github-dorker/Program.cs
+++ github-dorker/Program.cs
@@ -1,18 +1,61 @@
-﻿namespace GithubDorker
+﻿using Newtonsoft.Json;
+using System.Web;
+
+namespace GithubDorker
 {
     class GithubDorker
     {
+        const int batchSize = 30;
+
         static async Task Main(string[] args)
         {
-            var apiKey = Environment.GetEnvironmentVariable("GH_TOKEN");
-            var parameters = Environment.GetCommandLineArgs();
             var argumentParser = new ArgumentParser();
             var parsedArgs = argumentParser.ParseArguments(args);
 
-            foreach (var item in parsedArgs)
+            parsedArgs.TryGetValue("-df", out var files);
+            var dorkFilePath = files?.FirstOrDefault() ?? throw new ArgumentException("Dork file is not provided");
+            var dorkList = await GetSearchTermsAsync(dorkFilePath);
+
+            parsedArgs.TryGetValue("-t", out var tokens);
+            var parsedToken = tokens?.FirstOrDefault() ?? throw new ArgumentException("Token is not provided");
+            parsedArgs.TryGetValue("-org", out var organization);
+
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://api.github.com");
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"token {parsedToken}");
+
+            for (int i = 0; i < Math.Ceiling(dorkList.Count / (double)batchSize); i++)
             {
-                Console.WriteLine($"{item.Key}, [{string.Join(',', item.Value)}]");
+                var currentBatch = dorkList.Skip(i * batchSize)
+                    .Take(batchSize);
+
+                foreach (var item in currentBatch)
+                {
+                    Console.Write($"Searching: {item} ");
+                    var response = await httpClient.GetAsync($"/search/code?q={HttpUtility.UrlEncode(item)}+org:{organization.First()}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        dynamic result = JsonConvert.DeserializeObject(content);
+                        Console.WriteLine(result?.total_count);
+                    }
+
+                    Console.WriteLine();
+                }
+
+                await Task.Delay(60 * 1000);
             }
+
+        }
+
+        static async Task<List<string>> GetSearchTermsAsync(string dorkFilePath)
+        {
+            var result = File.Exists(dorkFilePath)
+                ? await File.ReadAllLinesAsync(dorkFilePath)
+                : Array.Empty<string>();
+
+            return result.ToList();
         }
     }
 }
